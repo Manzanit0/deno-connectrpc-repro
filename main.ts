119a1a0611350658bc75aed433db010e51967d8d
Javier García <unico.javier@gmail.com>
2024-09-23 15:42:06 +0200

    .

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { createPromiseClient } from "npm:@connectrpc/connect";
+import { createConnectTransport } from "npm:@connectrpc/connect-web";
+import { ElizaService } from "./api/gen/es/greet_connect.ts";
+
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("world"));
+
+
+  const transport = createConnectTransport({
+    baseUrl: "http://localhost:8083",
+  });
+  const client = createPromiseClient(ElizaService, transport);
+
+  await client.say({ sentence: "foo" });
